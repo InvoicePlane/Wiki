65745819e9938046d5cf9aa06e73b264878547d9
Kovah <mail@kovah.de>
2016-01-14 16:25:42 +0100

    Updated the FAQ page

--- wiki/en/1_0/general/faq.blade.php
+++ wiki/en/1_0/general/faq.blade.php
@@ -10,15 +10,13 @@
 
     <div id="faq">
 
-        <div class="alert alert-info hidden">Click on the Question to show the answer</div>
-
         <h3>General</h3>
 
-        <div id="general01" class="panel panel-default">
-            <div class="panel-heading">
+        <div id="general01" class="card">
+            <div class="card-header">
                 Can I sell / redistribute InvoicePlane?
             </div>
-            <div class="panel-body">
+            <div class="card-block">
                 <p class="text-success">InvoicePlane is a free software and it will never be a commercial product! We appeal everyone to respect the InvoicePlane project and our work and refrain from selling the application.<br/>
                     What you can do: offer paid professional support or hosting for InvoicePlane.</p>
                 <p>However InvoicePlane is an open source software released under <a href="http://choosealicense.com/licenses/mit/">MIT license</a>. This means that you can use the code <em>but</em> the InvoicePlane name and the logo are copyright by Invoiceplane.com and Kovah.de<br/>
@@ -28,57 +26,51 @@
             </div>
         </div>
 
-        <hr/>
-
         <h3>Settings</h3>
 
-        <div id="settings01" class="panel panel-default">
-            <div class="panel-heading">
+        <div id="settings01" class="card">
+            <div class="card-header">
                 Where can I set the default invoice / quote groups?
             </div>
-            <div class="panel-body">
+            <div class="card-block">
                 <p>You can set the default invoice / quote groups at <code>System settings > Invoices > Default Invoice Group</code> for invoices and <code>System settings > Quotes > Default Quote Group</code> for quotes.</p>
             </div>
         </div>
 
-        <hr/>
-
         <h3>Customization</h3>
 
-        <div id="custom01" class="panel panel-default">
-            <div class="panel-heading">
+        <div id="custom01" class="card">
+            <div class="card-header">
                 How can I customize the templates?
             </div>
-            <div class="panel-body">
+            <div class="card-block">
                 <p>You can find all templates in the following directories:</p>
                 <b>Invoices</b>
                 <p><code>/application/views/invoice_templates/</code></p>
                 <b>Quotes</b>
                 <p><code>/application/views/quote_templates/</code></p>
                 <p>The templates are using basic HTML, CSS and PHP. If you just want to change the styling you just need some knowledge of HTML and CSS.<br/>
-                If you want to include Custom Fields into the templates please refer to the <a href="/en/1.0/settings/custom-fields#add-to-template">Custom Fields page</a>.</p>
+                If you want to include Custom Fields into the templates please refer to the <a href="{{ url('en/1.0/settings/custom-fields#add-to-template') }}">Custom Fields page</a>.</p>
             </div>
         </div>
-
-        <hr/>
 
         <h3>Errors</h3>
 
-        <div id="error01" class="panel panel-default">
-            <div class="panel-heading">
+        <div id="error01" class="card">
+            <div class="card-header">
                 I copied InvoicePlane to my webserver but I get blank pages or an error 404 or 500
             </div>
-            <div class="panel-body">
+            <div class="card-block">
                 <p>Please make sure you copied the .htaccess file (hidden file on Unix systems) and you installed and enabled mod_rewrite for Apache.</p>
-                <p>If you want to install InvoicePlane in a sub-directory like <code>yourdomain.com/invoices/</code> please follow the instructions for the <a href="/en/1.0/getting-started/installation#subdir">installation in a subdirectory</a>.</p>
+                <p>If you want to install InvoicePlane in a sub-directory like <code>yourdomain.com/invoices/</code> please follow the instructions for the <a href="{{ url('en/1.0/getting-started/installation#subdir') }}">installation in a subdirectory</a>.</p>
             </div>
         </div>
 
-        <div id="error02" class="panel panel-default">
-            <div class="panel-heading">
+        <div id="error02" class="card">
+            <div class="card-header">
                 There is a large spinning cog (<i class="fa fa-cog"></i>) after I clicked a button and now nothing happens
             </div>
-            <div class="panel-body">
+            <div class="card-block">
                 <p>This problem occurs if the application is stuck because of an error.</p>
                 <ol>
                     <li>Open the file <code>/application/config/config.php</code> of your InvoicePlane installation.</li>
