cc9345d3ea9124519a8c69cb343895334fd3fba2
Kovah <mail@kovah.de>
2016-01-14 16:06:21 +0100

    Updated the root page and added dropdown to the version-sidebar

--- wiki/en/1_0/sidebar.blade.php
+++ wiki/en/1_0/sidebar.blade.php
@@ -1,18 +1,79 @@
 <li>
-    <a href="{{ url('en/1.0/general') }}">General information</a>
+    <a href="#" data-toggle="collapse" data-target="#submenu-10-general" class="has-submenu collapsed">
+        General information
+        <span class="pull-right"><span class="menu-icon fa fa-fw"></span></span>
+    </a>
+    <ul id="submenu-10-general" class="submenu collapse">
+        <li><a href="{{ url('en/1.0/general/about') }}">What is InvoicePlane?</a></li>
+        <li><a href="{{ url('en/1.0/general/changelog') }}">Changelog</a></li>
+        <li><a href="{{ url('en/1.0/general/license') }}">License</a></li>
+        <li><a href="{{ url('en/1.0/general/faq') }}">FAQ</a></li>
+    </ul>
 </li>
 <li>
-    <a href="{{ url('en/1.0/getting-started') }}">Getting Started</a>
+    <a href="#" data-toggle="collapse" data-target="#submenu-10-getting-started" class="has-submenu collapsed">
+        Getting started
+        <span class="pull-right"><span class="menu-icon fa fa-fw"></span></span>
+    </a>
+    <ul id="submenu-10-getting-started" class="submenu collapse">
+        <li><a href="{{ url('en/1.0/getting-started/requirements') }}">Requirements</a></li>
+        <li><a href="{{ url('en/1.0/getting-started/installation') }}">Installation</a></li>
+        <li><a href="{{ url('en/1.0/getting-started/quickstart') }}">Quickstart (Tutorial)</a></li>
+        <li><a href="{{ url('en/1.0/getting-started/updating-ip') }}">Updating InvoicePlane</a></li>
+    </ul>
 </li>
 <li>
-    <a href="{{ url('en/1.0/modules') }}">Modules</a>
+    <a href="#" data-toggle="collapse" data-target="#submenu-10-modules" class="has-submenu collapsed">
+        Modules
+        <span class="pull-right"><span class="menu-icon fa fa-fw"></span></span>
+    </a>
+    <ul id="submenu-10-modules" class="submenu collapse">
+        <li><a href="{{ url('en/1.0/modules/clients') }}">Clients</a></li>
+        <li><a href="{{ url('en/1.0/modules/quotes') }}">Quotes</a></li>
+        <li><a href="{{ url('en/1.0/modules/invoices') }}">Invoices</a></li>
+        <li><a href="{{ url('en/1.0/modules/recurring-invoices') }}">Recurring Invoices</a></li>
+        <li><a href="{{ url('en/1.0/modules/payments') }}">Payments</a></li>
+    </ul>
 </li>
 <li>
-    <a href="{{ url('en/1.0/settings') }}">Settings</a>
+    <a href="#" data-toggle="collapse" data-target="#submenu-10-settings" class="has-submenu collapsed">
+        Settings
+        <span class="pull-right"><span class="menu-icon fa fa-fw"></span></span>
+    </a>
+    <ul id="submenu-10-settings" class="submenu collapse">
+        <li><a href="{{ url('en/1.0/settings/general') }}">General Settings</a></li>
+        <li><a href="{{ url('en/1.0/settings/invoices') }}">Invoice Settings</a></li>
+        <li><a href="{{ url('en/1.0/settings/quotes') }}">Quotes Settings</a></li>
+        <li><a href="{{ url('en/1.0/settings/taxes') }}">Tax Settings</a></li>
+        <li><a href="{{ url('en/1.0/settings/email') }}">eMail Settings</a></li>
+        <li><a href="{{ url('en/1.0/settings/merchant-account') }}">Merchant Account</a></li>
+        <li><a href="{{ url('en/1.0/settings/updatecheck') }}">Updatecheck</a></li>
+        <li><a href="{{ url('en/1.0/settings/custom-fields') }}">Custom Fields</a></li>
+        <li><a href="{{ url('en/1.0/settings/email-templates') }}">eMail Templates</a></li>
+        <li><a href="{{ url('en/1.0/settings/invoice-groups') }}">Invoice Groups</a></li>
+        <li><a href="{{ url('en/1.0/settings/payment-methods') }}">Payment Methods</a></li>
+        <li><a href="{{ url('en/1.0/settings/taxrates') }}">Taxrates</a></li>
+        <li><a href="{{ url('en/1.0/settings/user-accounts') }}">User Accounts</a></li>
+    </ul>
 </li>
 <li>
-    <a href="{{ url('en/1.0/templates') }}">Templates</a>
+    <a href="#" data-toggle="collapse" data-target="#submenu-10-templates" class="has-submenu collapsed">
+        Templates
+        <span class="pull-right"><span class="menu-icon fa fa-fw"></span></span>
+    </a>
+    <ul id="submenu-10-templates" class="submenu collapse">
+        <li><a href="{{ url('en/1.0/templates/using-templates') }}">Using Templates</a></li>
+        <li><a href="{{ url('en/1.0/templates/customize-templates') }}">Customize Templates</a></li>
+    </ul>
 </li>
 <li>
-    <a href="{{ url('en/1.0/system') }}">System</a>
+    <a href="#" data-toggle="collapse" data-target="#submenu-10-system" class="has-submenu collapsed">
+        System
+        <span class="pull-right"><span class="menu-icon fa fa-fw"></span></span>
+    </a>
+    <ul id="submenu-10-system" class="submenu collapse">
+        <li><a href="{{ url('en/1.0/system/translation-localization') }}">Translation / Localization</a></li>
+        <li><a href="{{ url('en/1.0/system/importing-data') }}">Importing Data</a></li>
+        <li><a href="{{ url('en/1.0/system/upgrade-from-fusioninvoice') }}">Upgrade from FusionInvoice</a></li>
+    </ul>
 </li>
