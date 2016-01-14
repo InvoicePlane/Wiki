662e617ebd5fb8a3932050c23f64edef3d9e4dcd
Kovah <mail@kovah.de>
2016-01-14 15:29:37 +0100

    Optimized the theme

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
     $(".sidebar-toggle").click(function(e){
         e.preventDefault();
-        $("#sidebar").toggleClass("show-sidebar");
+        $("body").toggleClass("sidebar-visible");
     });
 });
