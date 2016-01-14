81a448b5ab889366f524877410ed2d70d70a4da0
Kovah <mail@kovah.de>
2016-01-14 07:23:23 +0100

    Updated theme and footer

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,7 +1,5 @@
 <div id="footer">
 
-    <a href="#top"><i class="fa fa-caret-up"></i> @lang('global.top')</a>
-    <br>
-    &copy 2013-{{ date('Y') }} <a href="https://invoiceplane.com">InvoicePlane.com</a>
+    <a href="#top"><i class="fa fa-caret-up"></i> @lang('global.top')</a> - &copy 2013-{{ date('Y') }} <a href="https://invoiceplane.com">InvoicePlane.com</a>
     
 </div>
