d3acdcf7b73ab56341151d848011f0ceb4fb053c
Kovah <mail@kovah.de>
2016-02-03 14:21:05 +0100

    Updated the installation guide

--- wiki/en/1_0/getting_started/installation.blade.php
+++ wiki/en/1_0/getting_started/installation.blade.php
@@ -12,17 +12,30 @@
         less.</p>
 
     <ol>
-        <li><a href="https://invoiceplane.com/downloads">Download</a> and copy all files to your server.</li>
+        <li><a href="https://invoiceplane.com/downloads">Download</a> and extract the archive.</li>
         <li>Create an empty database on your web server.</li>
-        <li>Upload the InvoicePlane files to your web server, either into its own subdirectory or into the public root
+        <li>Upload the files to your web server, either into its own subdirectory or into the public root
             of the web server.
         </li>
-        <li>Run the InvoicePlane installer from your web browser and follow his instructions: <code>http://your-domain.com/setup</code>
+        <li>Run the InvoicePlane installer from your web browser and follow his instructions: <code>http://your-domain.com/index.php/setup</code>
         </li>
     </ol>
 
     <p>Once the installer finished, the installation is complete and you may log into InvoicePlane using the email
         address and password you have chosen during the installation.</p>
+
+    <h3 id="htaccess">
+        Remove index.php from the URL <?php IP::headlineLink('/en/1.0/getting-started/installation#htaccess'); ?>
+    </h3>
+
+    <ol>
+        <li>Make sure that <a href="https://go.invoiceplane.com/apachemodrewrite">mod_rewrite</a> is enabled on your web server.</li>
+        <li>Open the file <code>/application/config/config.php</code></li>
+        <li>Search for <code>$config['index_page'] = 'index.php';</code> in this file and replace it with <code>$config['index_page'] = '';</code></li>
+        <li>Rename the <code>htaccess</code> file to <code>.htaccess</code></li>
+    </ol>
+    
+    <p>Now open <code>http://your-domain.com/setup</code> to run the setup.</p>
 
     <h3 id="subdir">
         Run InvoicePlane in a sub directory <?php IP::headlineLink('/en/1.0/getting-started/installation#subdir'); ?>
