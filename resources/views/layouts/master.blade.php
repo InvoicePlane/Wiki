784b2c3c7363ae6775b491faf41547982084b5c5
Kovah <mail@kovah.de>
2016-02-13 08:58:18 +0100

    Updated tracking code for wiki

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -44,5 +44,23 @@
 <script src="{{ elixir('assets/js/dependencies.js') }}"></script>
 <script src="{{ elixir('assets/js/app.js') }}"></script>
 
+<!-- Piwik -->
+<script type="text/javascript">
+  var _paq = _paq || [];
+  _paq.push(["setDomains", ["wiki.invoiceplane.com","wiki.invoiceplane.com"]]);
+  _paq.push(["setDoNotTrack", true]);
+  _paq.push(['trackPageView']);
+  _paq.push(['enableLinkTracking']);
+  (function() {
+    var u="//analytics.invoiceplane.com/";
+    _paq.push(['setTrackerUrl', u+'piwik.php']);
+    _paq.push(['setSiteId', 3]);
+    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+    g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
+  })();
+</script>
+<noscript><p><img src="//analytics.invoiceplane.com/piwik.php?idsite=3" style="border:0;" alt="" /></p></noscript>
+<!-- End Piwik Code -->
+
 </body>
 </html>
