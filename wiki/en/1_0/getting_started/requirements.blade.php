5c373bc44de887cead7ba8b93e56a68ff28be79b
Kovah <mail@kovah.de>
2016-07-01 17:54:32 +0200

    Update requirements

--- wiki/en/1_0/getting_started/requirements.blade.php
+++ wiki/en/1_0/getting_started/requirements.blade.php
@@ -13,20 +13,19 @@
         <li>
             A webserver / webhost with the following specifications:
             <ul>
-                <li>PHP > 5.3.0</li>
-                <li>MySQL > 4.x</li>
-                <li>And Following PHP Extensions:</li>
+                <li>MySQL > 5.x</li>
+                <li>PHP > 5.4.0 with the following PHP extensions:</li>
                 <ul>
+                    <li>php-gd</li>
                     <li>php-hash</li>
-                    <li>php-mcrypt (<a href="http://aryo.lecture.ub.ac.id/easy-install-php-mcrypt-extension-on-ubuntu-linux/" class="ext">How-To</a>)</li>
-                    <li>php-mbstring</li>
-                    <li>php-mysqli</li>
                     <li>php-json</li>
-                    <li>php-zlib</li>
+                    <li>php-mbstring</li>
+                    <li>php-mcrypt (<a href="http://aryo.lecture.ub.ac.id/easy-install-php-mcrypt-extension-on-ubuntu-linux/" class="ext">How-To</a>)</li>
+                    <li>php-mysqli</li>
                     <li>php-openssl</li>
                     <li>php-recode</li>
                     <li>php-xmlrpc</li>
-                    <li>php-gd</li>
+                    <li>php-zlib</li>
                 </ul>
             </ul>
         </li>
