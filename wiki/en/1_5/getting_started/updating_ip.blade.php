4be59809f621a4fb3a70ecde96192efe6b1ae66e
Kovah <mail@kovah.de>
2017-04-23 20:09:20 +0200

    Optimizes the upgrade guide

--- wiki/en/1_5/getting_started/updating_ip.blade.php
+++ wiki/en/1_5/getting_started/updating_ip.blade.php
@@ -27,38 +27,49 @@
 
     <hr>
 
-    <h3>Upgrade to InvoicePlane 1.5.0 from a previous version</h3>
+    <h3 id="upgrade-from-previous">
+        Upgrade to InvoicePlane 1.5.0 from a previous version <?php IP::headlineLink('/en/1.5/getting-started/updating-ip#upgrade-from-previous'); ?>
+    </h3>
 
     <p>Version 1.5.0 comes with some changes that require your attention. Be very careful and keep in mind to make a backup before you start. Make sure to read all instructions completely as we do not support any broken installations because you didn't followed this guid.</p>
 
     <div class="alert alert-warning">Please follow the next steps <b>before</b> running the update / setup!</div>
 
-    <h4>General Upgrade</h4>
+    <h4 id="upgrade-general">
+        General Upgrade <?php IP::headlineLink('/en/1.5/getting-started/updating-ip#upgrade-general'); ?>
+    </h4>
 
     <ol>
-        <li>The most important change: InvoicePlane now uses a new configuration file that is completely independent from the code itself. This means you set the configuration file once and you will never have to worry about overwriting some important settings. To migrate the configuration, follow these steps:
+        <li>First of all, make a backup of your current installation in another folder you can easily access in the next steps.</li>
+        <li>Then, delete everything in your InvoicePlane folder. <b>Everything</b>. You have a backup, so don't worry. This is very important as many old files are now deprecated and may lead to problems if you keep them.</li>
+        <li>Now, copy the contents of the 1.5.0 package into the InvoicePlane folder.</li>
+        <li>We will migrate some of your configuration to the new files now. All actions will take place in your InvoicePlane folder.
             <ul>
                 <li>Make a copy of the <code>ipconfig.php.example</code> file and rename the copy to <code>ipconfig.php</code>.</li>
-                <li>Open the <code>index.php</code> file and copy your URL from the top of the file.</li>
-                <li>Open the <code>ipconfig.php</code> file and paste the URL into the <code>IP_URL</code> setting like this: <code>IP_URL=http://ip1.dev</code>.</li>
-                <li>Open the <code>application/config/database.php</code> file and copy your database credentials to a text file or note them somewhere else.</li>
+                <li>Open the old <code>index.php</code> file and copy your URL from the top of the file.</li>
+                <li>Open the <code>ipconfig.php</code> file and paste the URL into the <code>IP_URL</code> setting like this: <code>IP_URL=http://your-domain.com</code>.</li>
+                <li>Open the old <code>application/config/database.php</code> file and copy your database credentials to a text file or note them somewhere else.</li>
                 <li>Set these database credentials in the <code>ipconfig.php</code> file. The database host goes into the <code>DB_HOSTNAME</code> setting like this: <code>DB_HOSTNAME=localhost</code>. All other database settings are set like this. The database port may not be set, so manually change it to <code>DB_PORT=3306</code>.</li>
             </ul>
+            <p class="alert alert-warning">Make sure you follow he instructions in the <code>ipconfig.php</code> file if you use a subdomain or a subfolder to run InvoicePlane.</p>
         </li>
-        <li>If you modified any other files like the tempaltes or any mPDF settings, make backups of these files in another directory!</li>
-        <li>Now delete all files within the InvoicePlane directory as many files and folders are now deprecated. Leaving all old files where they are may lead to problems! The setup will not clean up old files on his own.</li>
-        <li>Custom fields were overhauled and are now stored in an entirely other way than before. The setup will convert all existing custom fields. However, double-check if all fields were correctly migrated and if all values are still present. If not, report this bug to the community forums with as much information as possible.</li>
-        <li>If you used the merchant settings for online payments before, you have to enter all credentials again as they can't be migrated automatically.
-            <br>You can now select from a list of online payment providers. See the <a href="/en/1.5/settings/online-payments">Online Payment</a> page for more information.</li>
+        <li>You can now run the setup by opening <code>IP_URL=http://your-domain.com/index.php/setup</code> in your browser. The setup will then upgrade the database, migrate the custom fields and set a new encription key for you.</li>
     </ol>
 
-    <h4>Other notable changes</h4>
+    <p>Please notice: if you used custom templates or modified other files, take a look at the following section.</p>
+
+
+    <h4 id="other-changes">
+        Other notable changes <?php IP::headlineLink('/en/1.5/getting-started/updating-ip#other-changes'); ?>
+    </h4>
 
     <p>The following changes may not affect all users as they address specific parts of the application.</p>
 
     <ul>
-        <li>The variable `$show-discounts` was renamed to `$show-item-discounts` for PDF templates. If you use the variable, please update it. Please review the
-            <a href="/en/1.5/templates/customize-templates">Customize Templates</a> page for more information about the variables.</li>
+        <li>Custom fields were overhauled and are now stored in an entirely other way than before. The setup will convert all existing custom fields. However, double-check if all fields were correctly migrated and if all values are still present. If not, report this bug to the community forums with as much information as possible.</li>
+        <li>If you used the merchant settings for online payments before, you have to enter all credentials again as they can't be migrated automatically.
+            <br>You can now select from a list of online payment providers. See the <a href="/en/1.5/settings/online-payments">Online Payment</a> page for more information.</li>
+        <li>The variable <code>$show-discounts</code> was renamed to <code>$show-item-discounts</code> for PDF templates. If you use the variable, please update it. Please review the <a href="/en/1.5/templates/customize-templates">Customize Templates</a> page for more information about the variables.</li>
         <li>The files for custom CSS (`custom.css` and `custom-pdf.css`) moved to the `assets/core/css` folder. Make sure you transfer your custom styles to these files.</li>
         <li>If you modified any mPDF settings or added custom fonts, you have to copy these changes to the new location of mPDF which is now stored in this directory:
             <br><code>/vendor/mpdf/mpdf</code></li>
