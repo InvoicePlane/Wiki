ba71c03c776e0decdc291df637822aa2ffb89c63
naui95 <nahuel.guidotti@outlook.com>
2022-12-08 16:09:35 +0100

    added stripe instructions

--- wiki/en/1_6/settings/online_payments.blade.php
+++ wiki/en/1_6/settings/online_payments.blade.php
@@ -6,23 +6,29 @@
 
 @section('content')
 
-    <h2 class="page-title">Merchant Account Settings</h2>
-
     <h2 class="page-title">Online Payment Settings</h2>
 
     <p>InvoicePlane 1.6 supports only <b>Stripe</b> by default as a payment gateway. The reason why the other providers were dropped can be fount <a href="/en/1.6/getting-started/updating-ip#16-breaking-changes" target="_blank">here</a>. Please let us know what payment method you are missing at <a href="https://github.com/InvoicePlane/InvoicePlane/issues" target="_blank">GitHub</a></p>
 
-    <h3>Configure your payment provider</h3>
-
+    <h4>Configure your payment provider</h4>
     <p>To configure your payment provider, select the provider from the dropdown list. If you don't know which provider should be selected please contact your provider. We cannot offer any support for specific provider settings.</p>
-
     <p>After selecting a provider InvoicePlane will show you a configuration box will all needed settings. Make sure to enable the provider first. After that fill all needed information. Again: if you are not shure which fields to fill, contact your provider.</p>
 
     <div class="alert alert-warning">It is highly recommended to test if the online payment is working correctly using the <b>Test Mode</b> if available. This allows you to pay online without transfering real money.</div>
 
-    <h2>Missing providers</h2>
-
-    <p>At the moment InvoicePlane only supports some of all available payment providers. We may add support for custom providers in the future.</p>
+    <h4>Configure <b>Stripe</b> as a payment provider</h4>
+    <ol>
+        <li>Login or regiser for an account at <a href="https://stripe.com" target="_blank">stripe.com</a></li>
+        <li>Once you logged in, on the top search bar look for the <code>API Keys</code> page.</li>
+        <li>Create a new <i>secret key</i>.</li>
+        <li>Now, open the <code>settings</code> in your InvoicePlane installation and navigate to the <code>online payment</code> tab.</li>
+        <li>Tick the <code>Enable Online Payments</code> checkbox.</li>
+        <li>From the dropdown list select <code>Stripe</code> as a payment provider.</li>
+        <li>Tick the <code>enable</code> checkbox on the stripe card that appeared when you selected stripe as a payment provider in the last step.</li>
+        <li>Insert the secret key and the publishable key in the corrisponding fields. The secret key must be set in the field that hides the characters (like a password).</li>
+        <li>Click on <b>save</b>, and you are done!</li>
+    </ol>
+    <p><small>Please take note that InvoicePlane is in no way affiliated to Stripe and that we are not able to help with Stripe specific issues. These instructions only concern integrating Stripe as a payment gateway in InvoicePlane.</small></p>
 
     <?php
     $article_pagination = array(
